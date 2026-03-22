7824be0c4b73b6dd48171fcdf7db8e9e7776d81a
Synchyna Nazar <nsynchyna@gmail.com>
2026-03-22 14:29:11 +0200

    styling works_slider 360

--- src/js/script.js
+++ src/js/script.js
@@ -1,41 +1,39 @@
-import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
 
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 import "/src/sass/style.scss";
 
 try {
-    const swiper = new Swiper('.works__slider', {
-  slidesPerView: 1,
-  loop: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+  new Swiper(".works__slider", {
+    slidesPerView: 1,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
 
-  mousewheel: true,
-  keyboard: true,
+    mousewheel: true,
+    keyboard: true,
 
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
     breakpoints: {
-    1200: {
-      slidesPerView: 3,
-      spaceBetween: 5
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 5,
+      },
+
+      1920: {
+        spaceBetween: 35,
+      },
     },
 
-    1920: {
-      spaceBetween: 35,
-    },
-  },
-
-  modules: [Navigation, Pagination],
-});
-} catch(e) {
-
-}
+    modules: [Navigation, Pagination],
+  });
+} catch (e) {}
