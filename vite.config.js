f38952c32037e0dbdc2cbd90df6b9c650b7ba6c2
Synchyna Nazar <nsynchyna@gmail.com>
2026-06-20 18:12:20 +0300

    base catalog html

--- vite.config.js
+++ vite.config.js
@@ -8,8 +8,9 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
+        main: resolve(__dirname, "index.html"),
+        catalog: resolve(__dirname, "catalog.html"),
       },
     },
   },
-})
+});
